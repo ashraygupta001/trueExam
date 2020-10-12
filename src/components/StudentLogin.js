5e5cf79952abd131cf1332655251d652c8824edc
Ashray <ashray79gupta@gmail.com>
2020-10-12 16:37:43 +0530

    Almost done

--- src/components/StudentLogin.js
+++ src/components/StudentLogin.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './StudentLogin.module.scss';
 import { Button, Card, Form,Row,Col,Container } from 'react-bootstrap';
 import login from "./../assets/login.jpg";
@@ -12,27 +12,6 @@ const StudentLogin = () => {
     setStudent(false)
   }
   return (
-<<<<<<< HEAD
-    <Fragment>
-      <div className={styles.page} >
-        <div className={styles.inner}>
-          <div className={styles.logo}>
-            <Link to='/'>
-              <p className={styles.linkLogo}>
-                Editing-Bootcamp
-              </p>
-            </Link>
-          </div>
-          <div className={styles.image}>
-            <img src={login} alt="login" />
-          </div>
-          <div className={styles.welcomeLogo}>
-            <p>Welcome Back <span aria-label="happy" role="img">😀</span></p>
-          </div>
-          <div className={styles.welcomeContent}>
-            <div className={styles.ranContent}>
-              <p>Thanks For joining us On this amazing jounery of Editing.<span role="img" aria-label="happy">🎉</span> </p>
-=======
     <div className={styles.page}>  
     <Container fluid>
        <Row>
@@ -41,7 +20,6 @@ const StudentLogin = () => {
             <Card.Body>
               <div className={styles.logo}>
             Hellow world
->>>>>>> f41e98dd641bc138dced48e68fce516d8af3dd2f
             </div>
                 <Container fluid>
                   <Row>
@@ -134,96 +112,7 @@ const StudentLogin = () => {
         </Col>
       </Row>
     </Container>
-        {/* <div className={styles.upperbox}>
-            hello world
-        </div> */}
     </div>
-    // <Fragment>
-    //   <div className={styles.page} >
-    //     <div className={styles.inner}>
-    //       <div className={styles.logo}>
-    //         <p>
-    //           Editing-Bootcamp
-    //         </p>
-    //       </div>
-    //       <div className={styles.image}>
-    //         <img src={login} alt="login" />
-    //       </div>
-    //       <div className={styles.welcomeLogo}>
-    //         <p>Welcome Back <span aria-label="happy" role="img">😀</span></p>
-    //       </div>
-    //       <div className={styles.welcomeContent}>
-    //         <div className={styles.ranContent}>
-    //           <p>Thanks For joining us On this amazing jounery of Editing.<span role="img" aria-label="happy">🎉</span> </p>
-    //         </div>
-    //         <div className={styles.loginOptions}>
-    //           <div style={{justifySelf:"end", alignSelf:"center", paddingRight:20}}>
-    //             <Button variant="outline-primary" onClick={studentClick}>Student-Login{ student && <span role="img" aria-label="here">📌</span> }</Button>
-    //           </div>
-    //           <div style={{justifySelf:"start", alignSelf:"center",paddingLeft:20}}>
-    //            <Button variant="outline-primary" onClick={teacherClick}>Instructor-Login{ !student && <span role="img" aria-label="here">📌</span> }</Button>
-    //           </div>
-    //           {
-    //             student && (
-    //               <div className={styles.loginForm}>
-    //             <Form>
-    //             <Form.Group controlId="formBasicEmail">
-    //               <Form.Label>Email address</Form.Label>
-    //               <Form.Control type="email" placeholder="Enter email" />
-    //               <Form.Text className="text-muted">
-    //                 We'll never share your email with anyone else.
-    //               </Form.Text>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicPassword">
-    //               <Form.Label>Password</Form.Label>
-    //               <Form.Control type="password" placeholder="Password" />
-    //             </Form.Group>
-    //             <Form.Group controlId="formBasicCheckbox">
-    //               <Form.Check type="checkbox" label="Remember me" />
-    //                   </Form.Group>
-    //                   <Link to='/stuent-dashboard'>
-    //                     <Button variant="info" type="submit">
-    //                               Submit
-    //                     </Button>
-    //                   </Link>
-    //           </Form>
-    //           </div>
-    //             )
-    //           }
-    //           {
-    //             !student && (
-    //               <div className={styles.loginForm}>
-    //             <Form>
-    //             <Form.Group controlId="formBasicEmail">
-    //               <Form.Label>Email address</Form.Label>
-    //               <Form.Control type="email" placeholder="Enter email" />
-    //               <Form.Text className="text-muted">
-    //                 We'll never share your email with anyone else.
-    //               </Form.Text>
-    //             </Form.Group>
-
-    //             <Form.Group controlId="formBasicPassword">
-    //               <Form.Label>Password</Form.Label>
-    //               <Form.Control type="password" placeholder="Password" />
-    //             </Form.Group>
-    //             <Form.Group controlId="formBasicCheckbox">
-    //               <Form.Check type="checkbox" label="Remember me" />
-    //             </Form.Group>
-    //             <Link to='/instructor-dashboard'>
-    //                     <Button variant="info" type="submit">
-    //                               Submit
-    //                     </Button>
-    //             </Link>
-    //           </Form>
-    //           </div>
-    //             )
-    //           }
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </Fragment>
   )
 }
 
